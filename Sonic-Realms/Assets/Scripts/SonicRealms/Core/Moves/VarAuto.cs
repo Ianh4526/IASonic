b014370254213883fe3d6bf537774a1019c1373e
Ianh4526 <ianh4526@gmail.com>
2017-04-30 20:02:14 -0500

    Proyecto de IA

---
+++ Sonic-Realms/Assets/Scripts/SonicRealms/Core/Moves/VarAuto.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using SonicRealms.Core.Actors;
+using SonicRealms.Core.Utils;
+using UnityEngine;
+using SonicRealms.Core.Moves;
+
+public class VarAuto : MonoBehaviour {
+
+    #region Control
+    /// <summary>
+    /// The direction of the movement 1 or -1.
+    /// </summary>
+    [ControlFoldout]
+    [Tooltip("The direcation of the movement.")]
+    public float AutoMovementAxis;
+    #endregion
+
+    public static float VarAutoMov;
+    
+
+    // Use this for initialization
+    void Start () {
+        VarAutoMov = AutoMovementAxis;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+}
+}
